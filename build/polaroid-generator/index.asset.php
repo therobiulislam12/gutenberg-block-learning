1394da5e7c89679ea2b19b4fc28a37b8c31bbe99
therobiulislam12 <therobiulislam12@gmail.com>
2025-05-14 01:58:07 +0600

    Added rangecontrol for photos loaded

--- build/polaroid-generator/index.asset.php
+++ build/polaroid-generator/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4199bcd1a2b298fc1864');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bfb9ced0ff0ff757a4e3');
