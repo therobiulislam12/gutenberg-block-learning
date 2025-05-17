f8f7b6539bf4af2623cf01b73411245ac50da076
therobiulislam12 <therobiulislam12@gmail.com>
2025-05-17 18:47:40 +0600

    Render PHP dynamic Code is done

--- src/polaroid-generator/render.php
+++ src/polaroid-generator/render.php
@@ -1,8 +1,19 @@
 <?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
+
+$photos = $attributes['photos'];
+
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Polaroid Generator – hello from a dynamic block!', 'polaroid-generator' ); ?>
-</p>
+<section <?php echo get_block_wrapper_attributes(); ?>>
+	<div class="polaroid-container">
+		<?php for($i = 1; $i <= $photos; $i++) : ?>
+			<div className="polaroid">
+					<img 
+						width="200"
+						loading="lazy"
+						alt=""
+						src="<?php echo esc_url(add_query_arg('random', $i, 'https://picsum.photos/200/150')) ?>"
+					/>
+			</div>
+		<?php endfor; ?>
+	</div>
+</section>
